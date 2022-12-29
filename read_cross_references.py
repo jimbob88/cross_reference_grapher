d3d6cda28ecc358543fa891568aefe40593ef58d
James Blackburn <blackburnfjames@gmail.com>
2022-12-29 18:12:48 +0000

    Simple docs for initial functions

--- read_cross_references.py
+++ read_cross_references.py
@@ -18,6 +18,12 @@ def raw_interp(cross_references: str, ignore_first_row=False) -> List[Tuple[str,
 
 
 def verse_str_to_verse_pointer(verse_str: str) -> VersePointer:
+    """Takes a verse string and converts it to a named pointer to that same information.
+
+    Intended to improve legibility of code.
+
+    :param verse_str: For example 'Gen.1.1'
+    """
     book, chapter, verse = verse_str.split('.')
     return VersePointer(
         book_name=book, chapter=int(chapter), verse=int(verse)
@@ -25,6 +31,11 @@ def verse_str_to_verse_pointer(verse_str: str) -> VersePointer:
 
 
 def raw_interp_to_objects(cross_references: List[Tuple[str, str, int]]) -> List[OpenBibleCrossReference]:
+    """Converts the raw read of cross_references to a cleaner input
+
+    :param cross_references: The return of raw_interp()
+    :return: A list of objects which describe each bible cross_reference.
+    """
     return [OpenBibleCrossReference(
         from_verse=verse_str_to_verse_pointer(cross_reference[0]),
         to_verse=verse_str_to_verse_pointer(cross_reference[1]),
